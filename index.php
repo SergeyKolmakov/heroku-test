67d3038e6ee7448c27899644a4789596b0415534
Kolmakov Sergey <s-consul@mail.ru>
2018-06-25 03:05:20 +0400

    PHP test type hint

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?
 
-echo 'test';
-echo ' is OK';
+function add(int $a = 0, int $b = 0): int {
+  return $a + $b;
+}
+
+
+
+class User {
+
+  private $name;
+
+  public function __construct(string $name) {
+    $this->name = $name;
+  }
+}
+
+
+echo add(5, 3);
+echo add(7, 0);
